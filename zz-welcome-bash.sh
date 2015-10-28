eb857c4a5fc6ee9278acec7c79d964ba1ef780e5
killermoehre <killermoehre@gmx.net>
2015-10-28 01:24:54 +0100

    complete rewrite in bash

--- zz-welcome-bash.sh
+++ zz-welcome-bash.sh
@@ -4,70 +4,65 @@
 #
 # Maintainer:	danielschier84<at>gmail.com
 # Comitter:	killermoehre<at>gmx.net^
-# Version: 0.3
-gawk '
-BEGIN {
-	HOSTCOMMAND="hostname -f"
-	if ((HOSTCOMMAND | getline HOSTNAME) > 0) {
-		close(HOSTCOMMAND)
-	UNAMECOMMAND="uname -sr"
-	if ((UNAMECOMMAND | getline KERNEL) > 0)
-		close(UNAMECOMMAND)
-	}
-}
+# Version: 0.4
 
-{
-	if ( FILENAME == "/proc/cpuinfo" ) {
-		FS=":"
-		if ($1 ~ /model name/) {
-			#split($2, MODEL, " ")
-			#CPUMODEL = MODEL[1]" "MODEL[2]" "MODEL[3]
-			sub(/^ /, "", $2)
-			sub(/  .+/, "", $2)
-			CPUMODEL = $2
-		}
-		if ($1 ~ /cpu cores/) {
-			CPUCORES = $2
-			CPUTHREADS++
-		}
-		else {
-			if ($1 ~ /processor/) {
-				CPUCORES++
-				CPUTHREADS++
-			}
-		}
-		if ($1 ~ /cpu MHz/) {
-			gsub(/ /, "", $2)
-			CPUFREQUENCY = $2 / 1000
-		}
-	}
-	if ( FILENAME == "/proc/loadavg" ) {
-		split($1, LOADAVG, " ")
-	}
-	if ( FILENAME == "/proc/meminfo" ) {
-		FS=":"
-		sub(/ kB/, "", $2)
-		gsub(/ /, "", $2)
-		MEMINFO[$1] = $2
-	}
-}
+shopt -s extglob
 
-END {
-	printf(" System summary (collected %s)\n\
----------------------------------------------------------------\n\
- Hostname		= %s\n\
- Kernel			= %s\n\
-\n\
- CPU-Model		= %s\n\
- CPU-Frequency		= %.2f GHz\n\
- CPU-Cores/Threads	= %i/%i\n\
-\n\
- Load (1, 5, 15)	= %.2f %.2f %.2f\n\
-\n\
- Memory (free)		= %8i kiB\n\
- Memory (available)	= %8i kiB\n\
- Memory (total)		= %8i kiB\n\
- Swap (free)		= %8i kiB\n\
- Swap (total)		= %8i kiB\n\
----------------------------------------------------------------\n", strftime("%H:%M:%S %d.%m.%Y"), HOSTNAME, KERNEL, CPUMODEL, CPUFREQUENCY, CPUCORES, CPUTHREADS, LOADAVG[1], LOADAVG[2], LOADAVG[3], MEMINFO["MemFree"], MEMINFO["MemAvailable"], MEMINFO["MemTotal"], MEMINFO["SwapFree"], MEMINFO["SwapTotal"])
-}' /proc/cpuinfo /proc/loadavg /proc/meminfo 2> /dev/null || printf "No gawk available. Can't show welcome message." >&2
+FQDN="$(hostname -f)"
+UNAME="$(uname -sr)"
+
+declare -A CPUINFO MEMINFO
+declare -a LOADAVG
+
+#CPUINFO['processor']=1
+
+while IFS="	:" read -r OPTION VALUE; do
+	OPTION="${OPTION:=none}"
+	VALUE="${VALUE:=none}"
+	VALUE="${VALUE# }"
+	case "${OPTION}" in
+		processor)
+			printf "%i\n" "${CPUINFO[processor]}"
+			CPUINFO["$OPTION"]="$((CPUINFO[OPTION]++))"
+			continue
+			;;
+		'model name')
+			IFS="@" read -r CPUINFO["$OPTION"] CPUINFO['frequency'] <<< "$VALUE"
+			CPUINFO["$OPTION"]="${CPUINFO["$OPTION"]% }"
+			CPUINFO['frequency']="${CPUINFO['frequency']# }"
+			continue
+			;;
+		none)
+			continue
+			;;
+		*)
+			CPUINFO[$OPTION]="${VALUE}"
+			continue
+			;;
+	esac
+done < /proc/cpuinfo
+
+while IFS=":" read -r OPTION VALUE; do
+	VALUE="${VALUE% kB}"
+	declare -i MEMINFO["$OPTION"]="$VALUE"
+done < /proc/meminfo
+
+read -r -a LOADAVG < /proc/loadavg
+
+printf ' System summary (collected %(%H:%M:%S %d.%m.%Y)T)
+---------------------------------------------------------------
+ Hostname		= %s
+ Kernel			= %s
+
+ CPU-Model		= %s
+ CPU-Frequency		= %s
+ CPU-Cores/Threads	= %i/%i
+
+ Load (1, 5, 15)	= %s %s %s
+
+ Memory (free)		= %i kiB
+ Memory (available)	= %i kiB
+ Memory (total)		= %i kiB
+ Swap (free)		= %i kiB
+ Swap (total)		= %i kiB
+---------------------------------------------------------------\n' "$(printf '%(%s)T')" "$FQDN" "$UNAME" "${CPUINFO['model name']}" "${CPUINFO[frequency]}" "${CPUINFO['cpu cores']}" "${CPUINFO[processor]}" "${LOADAVG[0]}" "${LOADAVG[1]}" "${LOADAVG[2]}" "${MEMINFO[MemFree]}" "${MEMINFO[MemAvailable]}" "${MEMINFO[MemTotal]}" "${MEMINFO[SwapFree]}" "${MEMINFO[SwapTotal]}"
