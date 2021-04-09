e6cf04bed7eda7d22d4b8492c53438aad3ef071b
Silvio Knizek <killrmoehre@gmx.net>
2021-04-09 20:08:32 +0200

    use launchctl if available, use systemctl if available

--- .local/share/bash/variable-SSH_AUTH_SOCK.sh
+++ .local/share/bash/variable-SSH_AUTH_SOCK.sh
@@ -1,4 +1,9 @@
 #!/hint/bash
 SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 declare -x SSH_AUTH_SOCK
-launchctl setenv SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
+if hash launchctl &> /dev/null; then
+    launchctl setenv SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
+fi
+if hash systemctl &> /dev/null; then
+    systemctl --user set-environment "SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+fi
