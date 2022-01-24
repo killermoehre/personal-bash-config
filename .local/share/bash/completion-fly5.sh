3fd4d3109b107cc12307dd6d566a0be32a14b06f
Silvio Knizek <silvio.knizek@sap.com>
2022-01-24 13:35:51 +0100

    differentiate between fly5 and fly7

--- .local/share/bash/completion-fly5.sh
+++ .local/share/bash/completion-fly5.sh
@@ -1,7 +1,8 @@
-_fly_compl() {
+#!/bash
+_fly5_compl() {
 	args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
 	local IFS=$'\n'
 	COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} "${args[@]}"))
 	return 0
 }
-complete -F _fly_compl fly
+complete -F _fly5_compl fly5
