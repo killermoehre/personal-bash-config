0bb8e15b6e80814a04d3dadf27deedd0291433be
Silvio Knizek <silvio.knizek@sap.com>
2022-05-01 21:55:23 +0200

    make PATH more dynamic

--- .local/share/bash/variable-PATH.sh
+++ .local/share/bash/variable-PATH.sh
@@ -1,1 +1,6 @@
-export PATH="$HOME/.local/bin:/usr/local/opt/util-linux/bin:/usr/local/opt/util-linux/sbin:/usr/local/opt/mysql-client/bin:/usr/local/sbin:$PATH"
+#!/bash
+declare _path=''
+for _path in "$HOME/.local/bin" /usr/local/bin /usr/local/sbin /opt/homebrew/bin /opt/homebrew/sbin; do
+    test -d "$_path" && PATH="$_path:$PATH"
+done
+export PATH
