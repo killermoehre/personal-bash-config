fea87261ebcf9fd9390897480fe283d1a095ff2a
Silvio Knizek <silvio.knizek@sap.com>
2022-05-02 12:02:56 +0200

    clone necessary repos

---
+++ .config/yadm/bootstrap.d/50-additional-repos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone git@github.wdf.sap.corp:cc/secrets.git "$HOME/development/ccee-secrets"
+git clone https://github.tools.sap/b1-managed-service-ccee/chrome-extensions.git "$HOME/delepment/chrome-extensions"
