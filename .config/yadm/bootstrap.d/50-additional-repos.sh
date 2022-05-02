788601adc3d76f9799a60b64ecf4eea1b9a10ad1
Silvio Knizek <silvio.knizek@sap.com>
2022-05-02 12:05:51 +0200

    add error handling for default repos

--- .config/yadm/bootstrap.d/50-additional-repos.sh
+++ .config/yadm/bootstrap.d/50-additional-repos.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-git clone git@github.wdf.sap.corp:cc/secrets.git "$HOME/development/ccee-secrets"
-git clone https://github.tools.sap/b1-managed-service-ccee/chrome-extensions.git "$HOME/delepment/chrome-extensions"
+if test -d "$HOME/development/ccee-secrets"; then
+    git -C "$HOME/development/ccee-secrets" pull
+else
+    git clone git@github.wdf.sap.corp:cc/secrets.git "$HOME/development/ccee-secrets"
+fi
+if test -d "$HOME/development/chrome-extensions"; then
+    git -C "$HOME/development/chrome-extensions" pull
+else
+    git clone https://github.tools.sap/b1-managed-service-ccee/chrome-extensions.git "$HOME/development/chrome-extensions"
+fi
