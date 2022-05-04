6dad57d1de1ef8f936d5302a378422d2e8d5b3cf
Silvio Knizek <killermoehre@gmx.net>
2022-05-04 19:38:44 +0200

    more linux stuff

--- .config/yadm/bootstrap.d/05-start-vpn.sh
+++ .config/yadm/bootstrap.d/05-start-vpn.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/usr/sbin/scutil --nc start "SAP Global"
-until [[ "$(/usr/sbin/scutil --nc status "SAP Global" | head -1)" == "Connected" ]]; do
-    echo "Connecting to SAP Global"
-    sleep 1
-done
-echo "Connected to SAP Global"
+if test -x /usr/bin/scutil; then
+    /usr/sbin/scutil --nc start "SAP Global"
+    until [[ "$(/usr/sbin/scutil --nc status "SAP Global" | head -1)" == "Connected" ]]; do
+        echo "Connecting to SAP Global"
+        sleep 1
+    done
+    echo "Connected to SAP Global"
+fi
