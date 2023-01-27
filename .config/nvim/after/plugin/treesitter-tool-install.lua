fdaba7e453a10f0e8f59501d9cf96935cc4f2747
Silvio Knizek <silvio.knizek@sap.com>
2023-01-27 16:23:22 +0100

    more settings for nvim-treesitter

--- .config/nvim/after/plugin/treesitter-tool-install.lua
+++ .config/nvim/after/plugin/treesitter-tool-install.lua
@@ -1,23 +1,17 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {
-        "bash",
-        "comment",
-        "diff",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "help",
-        "jq",
-        "lua",
-        "markdown",
-        "python",
-        "yaml",
-    },
-    sync_install = false,
+require('nvim-treesitter.configs').setup {
+    ensure_installed = "all",
     auto_install = true,
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
+    },
+    indent = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+    },
+    textobjects = {
+        enable = true,
     },
 }
