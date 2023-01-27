dfc4c704bc2b9d405e7e6fc4f1567a6df9c47346
Silvio Knizek <silvio.knizek@sap.com>
2023-01-27 16:22:26 +0100

    add config to recognize the global `vim` in lua

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -1,6 +1,7 @@
 local api = vim.api
 local g = vim.g
 local opt = vim.opt
+local cmd = vim.cmd
 
 -- Remap leader and local leader to <Space>
 api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
@@ -30,9 +31,13 @@ opt.showmode = true
 opt.modeline = true
 
 -- Highlight on yank
-vim.cmd [[
+cmd [[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
+]]
+
+cmd [[
+  autocmd BufWritePre * lua vim.lsp.buf.format()
 ]]
